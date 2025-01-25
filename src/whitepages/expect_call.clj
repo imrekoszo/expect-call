6511f699e91b79ecf6e995250e5abc9dd6176420
Imre Kószó <imrekoszo@users.noreply.github.com>
2025-01-25 01:15:37 +0100

    add, export, and use clj-kondo config & hook

--- src/whitepages/expect_call.clj
+++ src/whitepages/expect_call.clj
@@ -11,7 +11,11 @@
 
 ;; This is an alias
 (defmacro with-expect-call
-  "This is an alias for expect-call, with a with- prefix
+  "expected-fns: (fn arg-match body...)
+                 or [(fn arg-match body...), (fn arg-match body...)...]
+   Each fn may be preceded by keywords :more, :never or :do.
+
+   This is an alias for expect-call, with a with- prefix
    so emacs clojure-mode indents it more nicely"
-  [& args]
-  `(-expect-call ~@args))
+  [expected-fns & body]
+  `(-expect-call ~expected-fns ~@body))
