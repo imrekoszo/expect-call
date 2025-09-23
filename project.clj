7ea0df6edb15887fa9e84efbd7ef1b1d6055c008
Imre Kószó <imrekoszo@users.noreply.github.com>
2025-09-23 11:42:56 +0200

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject whitepages/expect-call "0.3.1" ;; NOTE: no longer published
+(defproject whitepages/expect-call "0.3.2" ;; NOTE: no longer published
   :description "A Clojure library for no-fuss function mocking"
   :url "https://github.com/whitepages/expect-call"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/core.match "1.0.0"]]
+  :dependencies [[org.clojure/core.match "1.1.0"]]
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
              :1.11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
-             :1.12 {:dependencies [[org.clojure/clojure "1.12.0"]]}})
+             :1.12 {:dependencies [[org.clojure/clojure "1.12.2"]]}})
